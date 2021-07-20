b71ccbc1a3b6779827292d5aebc90192cc09752c
MPx <hellomariap@gmail.com>
2021-07-20 14:39:41 -0700

    Added and fixed links issue

--- source/template.js
+++ source/template.js
@@ -10,7 +10,7 @@ const cardBuilder = team => {
     
       <ul class="list-group list-group-flush">
       <li class="list-group-item"><b>Id:</b> ${manager.getId()}</li>
-      <li class="list-group-item"><b>Email:</b> <a href="${manager.getEmail()}"></a></li>
+      <li class="list-group-item"><b>Email:</b> ${manager.getEmail()}</li>
       <li class="list-group-item"><b>Office Number:</b> ${manager.getOfficeNumber()}</li>
       </ul>
   </div>
@@ -43,7 +43,8 @@ const cardBuilder = team => {
       <ul class="list-group list-group-flush">
       <li class="list-group-item"><b>Id:</b> ${engineer.getId()}</li>
       <li class="list-group-item"><b>Email:</b> ${engineer.getEmail()}</li>
-      <li class="list-group-item"><b>GitHub:</b> <a href="${engineer.getGithub()}"></a></li>
+      <li class="list-group-item"><b>GitHub:</b> <a href=https://www.github.com/>${engineer.getGithub()}</a></li>
+      
       </ul>
   </div>
       `
